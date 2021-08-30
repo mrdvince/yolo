827b2b4afeeb5961e8fa66a97b6fee387b24db57
Vince <vincivenv@gmail.com>
2021-08-30 22:04:30 +0300

    detect objects, plot bounding boxes

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from os import linesep, name
 import time
 
 import matplotlib.patches as patches
@@ -73,3 +74,135 @@ def nms(boxes, iou_thresh):
                 if boxes_iou(box_i, box_j) > iou_thresh:
                     box_j[4] = 0
     return best_boxes
+
+
+def detect_objects(model, img, iou_thresh, nms_thresh):
+    start = time.time()
+    model.eval()
+    # image shape 1x3x416x416
+    img = torch.from_numpy(img.transpose(2, 0, 1)).float().div(255.0).unsqueeze()
+    list_boxes = model(img, nms_thresh)
+    # new list with all the boxes returned by the network
+    boxes = list_boxes[0][0] + list_boxes[1][0] + list_boxes[2][0]
+    # Perform the second step of NMS on the bounding boxes returned by the neural network.
+    # In this step, we only keep the best bounding boxes by eliminating all the bounding boxes
+    # whose IOU value is higher than the given IOU threshold
+    boxes = nms(boxes, iou_thresh)
+    # send time
+    finish = time.time()
+
+    # Print the time it took to detect objects
+    print(
+        "\n\nIt took {:.3f}".format(finish - start),
+        "seconds to detect the objects in the image.\n",
+    )
+
+    # Print the number of objects detected
+    print("Number of Objects Detected:", len(boxes), "\n")
+
+    return boxes
+
+
+def load_class_names(namesfile):
+    class_names = []
+    with open(namesfile, "r") as fp:
+        lines = fp.readlines()
+    # get object class names
+    for line in lines:
+        line = line.rstrip()
+        class_names.append(line)
+    return class_names
+
+
+def print_objects(boxes, class_names):
+    print("Objects Found and Confidence Level:\n")
+    for i in range(len(boxes)):
+        box = boxes[i]
+        if len(box) >= 7 and class_names:
+            cls_conf = box[5]
+            cls_id = box[6]
+            print("%i. %s: %f" % (i + 1, class_names[cls_id], cls_conf))
+
+
+def plot_boxes(img, boxes, class_names, plot_labels, color=None):
+    # Define a tensor used to set the colors of the bounding boxes
+    colors = torch.FloatTensor(
+        [[1, 0, 1], [0, 0, 1], [0, 1, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0]]
+    )
+
+    # set colors of the bounding boxes
+    def get_color(c, x, max_val):
+        ratio = float(x) / max_val * 5
+        i = int(np.floor(ratio))
+        j = int(np.ceil(ratio))
+
+        ratio = ratio - i
+        r = (1 - ratio) * colors[i][c] + ratio * colors[j][c]
+        return int(r * 255)
+
+    # width and height of the image
+    width = img.shape[1]
+    height = img.shape[0]
+
+    # figure and plot the image
+    fig, a = plt.subplots(1, 1)
+    a.imshow(img)
+
+    # plot the bounding boxes and corresponding labels
+    for i in range(len(boxes)):
+        box = boxes[i]
+        # Get the (x,y) pixel coordinates of the lower-left and lower-right corners
+        # of the bounding box relative to the size of the image.
+        x1 = int(np.around((box[0] - box[2] / 2.0) * width))
+        y1 = int(np.around((box[1] - box[3] / 2.0) * height))
+        x2 = int(np.around((box[0] + box[2] / 2.0) * width))
+        y2 = int(np.around((box[1] + box[3] / 2.0) * height))
+        # rgb value -> red
+        rgb = (1, 0, 0)
+
+        if len(box) >= 7 and class_names:
+            cls_conf = box[5]
+            cls_id = box[6]
+            classes = len(class_names)
+            offset = cls_id * 123457 % classes
+            red = get_color(2, offset, classes) / 255
+            green = get_color(1, offset, classes) / 255
+            blue = get_color(0, offset, classes) / 255
+
+            # if color
+            if color is None:
+                rgb = (red, green, blue)
+            else:
+                rgb = color
+
+        # width and height of the bounding box relative to size of image
+        width_x = x2 - x1
+        width_y = y1 - y2
+        # Set the postion and size of the bounding box. (x1, y2) is the pixel coordinate of the
+        # lower-left corner of the bounding box relative to the size of the image.
+        rect = patches.Rectangle(
+            (x1, y2), width_x, width_y, linewidth=2, edgecolor=rgb, facecolor="none"
+        )
+
+        # draw the bounding box on the image
+        a.add_patch(rect)
+
+        # if plot labels
+        if plot_labels:
+            # string with object class name and corresponding object class probability
+            conf_txt = class_names[cls_id] + ": {:.1f}".format(cls_conf)
+            # x and y offsets for the labels
+            lxc = (img.shape[1] * 0.266) / 100
+            lyc = (img.shape[0] * 1.180) / 100
+
+            # draw the labels on top of the image
+            a.text(
+                x1 + lxc,
+                y1 - lyc,
+                conf_txt,
+                fontsize=24,
+                color="k",
+                bbox=dict(facecolor=rgb, edgecolor=rgb, alpha=0.8),
+            )
+
+        plt.show()
